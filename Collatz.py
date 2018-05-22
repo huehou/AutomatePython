b09228538d61bef5dcb914e6ef493e3610516a0e
Alexander Hue <Alexander_hue1993@hotmail.com>
2018-05-22 13:32:30 +0800

    Collatz sequence program

---
+++ Collatz.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 13:25:44 2018
+
+@author: alexander
+"""
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number//2)
+        return number // 2
+    else:
+        print(3*number + 1)
+        return 3*number + 1
+
+print("Enter number: ")
+
+while True:
+    try:
+        number = int(input())
+        break
+    except ValueError:
+        print("You must enter an integer")
+
+while number != 1:
+    number = collatz(number)
